d8d1702ba48d03366eb9fa238ec8742086646188
Maleah <41594685+Mmristau@users.noreply.github.com>
2018-07-27 15:39:47 -0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){ 
+	console.log("Ready!");
+	var Arrays = ["I am One","I am Two","I am Three","I am Four","Guess what, I'm five!"];
+		console.log("So, this is an Array,(below this log within the console) in this case we have an array of string data types!");
+		console.log(Arrays);
+	console.log("The cool thing about arrays is that we can call for one value like:");
+	console.log(Arrays[0]);
+		console.log("Or put the whole");
+	
+	
+	
+	
+});
